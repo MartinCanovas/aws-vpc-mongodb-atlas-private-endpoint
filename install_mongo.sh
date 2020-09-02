c02f6e57082bc714ec3adf8d81909f65e29f9d6e
Martin Canovas <martin.canovas@mongodb.com>
2020-09-02 17:08:20 -0300

    Updating mongosh version to 0.2.2

--- install_mongo.sh
+++ install_mongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -OL https://downloads.mongodb.com/compass/mongosh-0.1.0-linux.tgz
-tar -zxvf mongosh-0.1.0-linux.tgz
+curl -OL https://downloads.mongodb.com/compass/mongosh-0.2.2-linux.tgz
+tar -zxvf mongosh-0.2.2-linux.tgz
 chmod +x mongosh
 sudo cp mongosh /usr/local/bin/
